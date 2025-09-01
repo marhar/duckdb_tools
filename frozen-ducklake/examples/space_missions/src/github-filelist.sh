aeceda5d2ef33260b389e016a8d656ab887b9f49
Mark Harrison <mark.harrison@madhive.com>
2025-09-01 09:57:40 -0700

    tidy up.

--- frozen-ducklake/examples/space_missions/src/github-filelist.sh
+++ frozen-ducklake/examples/space_missions/src/github-filelist.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+# github-filelist.sh -- recursively list all parquet files in a github directory
 
 TMPJ=/tmp/fetch.$$.json
 trap "rm -f $TMPJ" 0
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 github-url" 1>&2
-    echo "example: $0 https://github.com/marhar/duckdb_tools/tree/main/frozen-ducklake/examples/space_missions/data" 1>&2
-    exit 1
+  echo "Usage: $0 github-url" 1>&2
+  echo "example: $0 https://github.com/marhar/duckdb_tools/tree/main/frozen-ducklake/examples/space_missions/data" 1>&2
+  exit 1
 fi
+
+f() {
+  CPATH=https://api.github.com/repos/$REPO/contents/$DPATH
+  curl -s $CPATH > $TMPJ
+
+  # if row is a directory recurse into it, else print the download_url if it's a parquet file
+  for DPATH in $(duckdb -noheader -ascii -c "SELECT path FROM '$TMPJ' WHERE type='dir' ORDER BY name;"); do
+    f $REPO $DPATH
+  done
+  duckdb -noheader -ascii -c "select download_url from '$TMPJ' where type='file' and name like '%.parquet' ORDER BY name;"
+}
 
 URL=$1
 REPO=$(duckdb -noheader -ascii -c "SELECT array_to_string((split('$URL', '/'))[4:5], '/');")
 DPATH=$(duckdb -noheader -ascii -c "SELECT array_to_string((split('$URL', '/'))[8:], '/');")
-CPATH=https://api.github.com/repos/$REPO/contents/$DPATH
 
-curl -s $CPATH > $TMPJ
-
-# if row is a directory, recurse into it, else print the download_url if it's a parquet file
-for xdir in $(duckdb -noheader -ascii -c "select path from '$TMPJ' where type='dir';"); do
-    SUBDIR_URL=https://github.com/$REPO/tree/main/$xdir
-    $0 $SUBDIR_URL
-done
-duckdb -noheader -ascii -c "select download_url from '$TMPJ' where type='file' and name like '%.parquet';"
+f $REPO $DPATH
